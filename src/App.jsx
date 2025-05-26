1932bdf09f33599294b90846fdfe1f3beefd7341
radeinjac <radeinjac17@gmail.com>
2025-05-26 22:23:03 +0200

    Initial upload from working ZIP
    Initial upload from working ZIP

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Hello from WhoIsYou 🚀</h1>
+      <p>Your site is working!</p>
+    </div>
+  );
+}
